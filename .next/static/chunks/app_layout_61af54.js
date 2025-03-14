f921535ae4f2cff1001b1cdfe3bb2a2a4a6043b3
Manish Chaulagain <mchaulagain2s@semo.edu>
2025-03-13 19:41:41 -0500

    initial push

---
+++ .next/static/chunks/app_layout_61af54.js
@@ -0,0 +1,14 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_61af54.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_61af54.js",
+  "chunks": [
+    "static/chunks/[root of the server]__cdd99c._.css",
+    "static/chunks/node_modules_80aa37._.js",
+    "static/chunks/_0d3910._.js",
+    "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_00b1c3._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/app_page_jsx_61af54._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_page_jsx_61af54._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_page_jsx_61af54._.js",
+  "chunks": [
+    "static/chunks/_6e2d42._.js",
+    "static/chunks/node_modules_next_c832be._.js"
+  ],
+  "source": "dynamic"
+});
