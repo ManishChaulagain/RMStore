4ed3dd597f1387e75a110e0a2fb8c11e987d959f
Manish Chaulagain <mchaulagain2s@semo.edu>
2025-03-22 17:22:22 -0500

    fixed

--- config/inngest.js
+++ config/inngest.js
@@ -7,59 +7,60 @@ export const inngest = new Inngest({ id: "rmstore-next" });
 
 // Function to save user data to MongoDB
 export const syncUserCreation = inngest.createFunction(
-    {
-        id: 'sync-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.created'
-    },
-    async ({ event }) => {
-            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-            const userData = {
-                _id: id,
-                email: email_addresses[0].email_address,
-                name: first_name + " " + last_name,
-                imageUrl: image_url
-            };
-            await connectDB
-            await User.create(userData);
+      const userData = {
+        _id: id,
+        email: email_addresses?.[0]?.email_address || "",
+        name: `${first_name} ${last_name}`,
+        imageUrl: image_url,
+      };
+
+      await connectDB();
+      await User.create(userData);
+    } catch (error) {
+      console.error("Error in syncUserCreation:", error);
     }
-)
+  }
+);
 
 // Function to update user data in MongoDB
 export const syncUserUpdate = inngest.createFunction(
-    {
-        id: 'update-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.updated'
-    },
-    async ({ event }) => {
-            const { id, first_name, last_name, email_addresses, image_url } = event.data;
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
-            const userData = {
-                _id: id,
-                email: email_addresses[0].email_address,
-                name: first_name + " " + last_name,
-                imageUrl: image_url
-            };
-            await connectDB
-            await User.findByIdAndUpdate(id, userData);
+      const userData = {
+        email: email_addresses?.[0]?.email_address || "",
+        name: `${first_name} ${last_name}`,
+        imageUrl: image_url,
+      };
+
+      await connectDB();
+      await User.findByIdAndUpdate(id, userData);
+    } catch (error) {
+      console.error("Error in syncUserUpdate:", error);
     }
-)
+  }
+);
 
 // Function to delete user from MongoDB
 export const syncUserDelection = inngest.createFunction(
-    {
-        id: 'delete-user-from-clerk'
-    },
-    {
-        event: 'clerk/user.deleted'
-    },
-    async ({ event }) => {
-            const { id } = event.data;
-            await connectDB
-            await User.findByIdAndDelete(id);
+  { id: "delete-user-from-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    try {
+      const { id } = event.data;
+      await connectDB();
+      await User.findByIdAndDelete(id);
+    } catch (error) {
+      console.error("Error in syncUserDelection:", error);
     }
-)
+  }
+);
